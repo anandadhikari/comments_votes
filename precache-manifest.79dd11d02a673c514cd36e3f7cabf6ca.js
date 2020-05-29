c6e4ff067fce8a69ab111f3400126b67d0d74b04
Anand <anandadhikari02@gmail.com>
2020-05-29 17:25:37 +0530

    Updates

---
+++ precache-manifest.79dd11d02a673c514cd36e3f7cabf6ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024f0dd8cfd12e5c573bf0dd49fe8da4",
+    "url": "/rock_paper_scissor/index.html"
+  },
+  {
+    "revision": "862f5a5c5de1afaa2b65",
+    "url": "/rock_paper_scissor/static/css/main.48d3b07c.chunk.css"
+  },
+  {
+    "revision": "d7cd3ca01ebe2a89e3a4",
+    "url": "/rock_paper_scissor/static/js/2.776847f0.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/rock_paper_scissor/static/js/2.776847f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "862f5a5c5de1afaa2b65",
+    "url": "/rock_paper_scissor/static/js/main.5773c91f.chunk.js"
+  },
+  {
+    "revision": "a7f5d4167f183ebd3317",
+    "url": "/rock_paper_scissor/static/js/runtime-main.00a5161e.js"
+  }
+]);
